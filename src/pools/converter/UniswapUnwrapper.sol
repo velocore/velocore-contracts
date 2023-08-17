750d936c4aca78d6d024d17d727141da48c02994
Haruka <hauruka@velocore.xyz>
2023-08-17 14:12:29 +0900

    feat: UniswapUnwrapper

---
+++ src/pools/converter/UniswapUnwrapper.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "../PoolWithLPToken.sol";
+import "src/lib/RPow.sol";
+import "src/interfaces/IConverter.sol";
+import "openzeppelin-contracts/contracts/utils/math/SafeCast.sol";
+import "openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Upgrade.sol";
+
+interface IUniswapPair {
+    function burn(address to) external;
+}
+
+contract UniswapUnwrapper is IConverter, Pool {
+    using TokenLib for Token;
+    using SafeCast for uint256;
+    using SafeCast for int256;
+    using UncheckedMemory for Token[];
+    using UncheckedMemory for int128[];
+
+    constructor(IVault vault_) Pool(vault_, address(this), address(this)) {}
+
+    function velocore__convert(address, Token[] calldata t, int128[] memory r, bytes calldata) external onlyVault {
+        for (uint256 i = 0; i < t.length; i++) {
+            Token token = t.u(i);
+            int128 delta = r.u(i);
+            if (delta > 0) {
+                token.transferFrom(address(this), token.addr(), uint256(int256(delta)));
+                IUniswapPair(token.addr()).burn(address(vault));
+            }
+        }
+    }
+
+    receive() external payable {}
+}
